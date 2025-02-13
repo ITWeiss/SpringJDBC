ac2c346fb03ea11d4a837120e10a9fa34d83768d
ITWeiss <gottfried98@bk.ru>
2025-02-13 22:56:08 +0700

    Spring Configuration for DB,Spring,JDBC

--- src/main/java/com/example/spring/jdbc/Main.java
+++ src/main/java/com/example/spring/jdbc/Main.java
@@ -16,22 +16,22 @@ public class Main {
         AnnotationConfigApplicationContext context = new AnnotationConfigApplicationContext(AppConfig.class);
         ProductDao productDao = context.getBean(ProductDao.class);
 
-//        Product product1 = new Product (1, "Apple", 2.38);
-//        productDao.save(product1);
-//
-//        Product product2 = new Product(2, "Orange", 1.89);
-//        productDao.save(product2);
-//
-//        Product product3 = new Product(1, "Apple", 1.38);
-//        productDao.update(product3);
-//
-//        List<Product> users = productDao.findAll();
-//        users.forEach(p -> System.out.println(p.getName() + " - " + p.getPrice()));
-//
-//        Product product4 = new Product(3, "Lemon", 0.67);
-//        productDao.createProductWithLog(product4);
-//
-//        productDao.deleteById(1);
+        Product product1 = new Product (1, "Apple", 2.38);
+        productDao.save(product1);
+
+        Product product2 = new Product(2, "Orange", 1.89);
+        productDao.save(product2);
+
+        Product product3 = new Product(1, "Apple", 1.38);
+        productDao.update(product3);
+
+        List<Product> users = productDao.findAll();
+        users.forEach(p -> System.out.println(p.getName() + " - " + p.getPrice()));
+
+        Product product4 = new Product(3, "Lemon", 0.67);
+        productDao.createProductWithLog(product4);
+
+        productDao.deleteById(1);
 
     }
 }
